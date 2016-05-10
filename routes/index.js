5dfa8acaf6fad3f36f2973f4097a2fbbd1f4b829
ityn <ityn8161@gmail.com>
2016-05-10 17:15:16 +0700

    working

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var wp =require("./wp");
+//var wp2 =require("./wp2");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -13,5 +15,8 @@ router.get('/auth', function(req, res, next) {
 router.get('/auth_callback', function(req, res, next) {
   res.render('auth', { title: 'Auth_callback' });
 });
+
+router.get('/wall_post', wp);
+//router.get('/wall_post2', wp2);
 
 module.exports = router;
---
+++ routes/wp.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+
+// Setup
+var VK = require('vksdk');
+var vk = new VK({
+    'appId'     : 5080342,
+    'appSecret' : 'gaMQGLcOVBXwPBKGSSnD',
+    'language'  : 'ru',
+    'v': '5.52'
+});
+
+var access_token = "b27813b3256e60152929a322de2321ce3e8e98ab6053089b5124b188e36b6d23cbb272891a92ddca30baa";
+var u_id = 88567346;
+var club1 = -72688782;
+var club2 = -100659116;
+var scope = 337026;
+vk.setSecureRequests(true);
+vk.setToken(access_token);
+
+// Request 'users.get' method
+
+// Setup server access token for server API methods
+//vk.on('serverTokenReady', function(_o) {
+//    // Here will be server access token
+//    console.log(_o.access_token);
+//    vk.setToken(_o.access_token);
+//});
+//
+//// Turn on requests with access tokens
+//vk.setSecureRequests(true);
+//https://api.vk.com/method//wall.post?owner_id=-100659116&message=TEST#&v=5.52&access_token=b27813b3256e60152929a322de2321ce3e8e98ab6053089b5124b188e36b6d23cbb272891a92ddca30baa
+/* GET users listing. */
+router.get('/wall_post', function(req, res, next) {
+    //vk.request('users.get', {'user_id' : u_id}, function(_o) {
+    vk.request('wall.post', {'owner_id': club1, 'message': 'test2', 'guid': 'p123456'}, function(_o){
+        console.log(_o);
+        res.send('respond with a Wall_Post');
+    });
+});
+
+module.exports = router;
